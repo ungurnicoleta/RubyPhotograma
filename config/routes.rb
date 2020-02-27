2ccfdf6183447dff272d2f2e39e2052621849ee3
Cosmin Rusu <cr.rusucosmin@gmail.com>
2020-02-27 09:40:38 -0800

    integrate with devise auth token

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :api do
+    scope :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
+
   get 'welcome/index'
+  get :auth, to: 'application#authenticated_user'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'application#hello'
---
+++ db/migrate/20200227171113_devise_token_auth_create_users.rb
@@ -0,0 +1,9 @@
+class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :provider, :string, :null => false, :default => "email"
+    add_column :users, :uid, :string, :null => false, :default => ""
+    add_column :users, :tokens, :json
+
+    add_index :users, [:uid, :provider],     unique: true
+  end
+end
