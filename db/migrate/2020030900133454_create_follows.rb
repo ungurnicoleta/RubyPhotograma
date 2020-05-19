ecebc5694b31b82605476dd816ca8b053e510a52
Nicoleta Ungur <ungurnicoleta1998@gmail.com>
2020-05-20 00:28:36 +0300

    Rename migration

--- db/migrate/2020030900133454_create_follows.rb
+++ db/migrate/2020030900133454_create_follows.rb
@@ -2,7 +2,7 @@
 
 class CreateFollows < ActiveRecord::Migration[6.0]
   def change
-    create_table :addresses do |t|
+    create_table :follows do |t|
       t.integer :follower_id
       t.integer :followee_id
     end
