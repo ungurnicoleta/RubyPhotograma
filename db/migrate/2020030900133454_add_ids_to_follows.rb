fe0dcb019a6f2d996bf033a9648b81496aa9fe6d
Nicoleta Ungur <ungurnicoleta1998@gmail.com>
2020-05-19 23:24:24 +0300

    Modify gitignore

--- db/migrate/2020030900133454_add_ids_to_follows.rb
+++
@@ -1,9 +0,0 @@
-class AddIdsToFollows < ActiveRecord::Migration[6.0]
-  def change
-    add_column :follows, :follower_id, :integer
-    add_column :follows, :followee_id, :integer
-
-  end
-end
-
-
