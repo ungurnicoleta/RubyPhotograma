bef01fac8bfec8698a7d7d240d10c868ab606405
Nicoleta Ungur <ungurnicoleta1998@gmail.com>
2020-05-20 00:13:41 +0300

    Alter users table

--- db/migrate/20200227171113_devise_token_auth_create_users.rb
+++ db/migrate/20200227171113_devise_token_auth_create_users.rb
@@ -1,5 +1,8 @@
 class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[6.0]
   def change
+    add_column :users, :name, :string
+    add_column :users, :avatar, :string
+    add_column :users, :phone, :string
     add_column :users, :provider, :string, :null => false, :default => "email"
     add_column :users, :uid, :string, :null => false, :default => ""
     add_column :users, :tokens, :json
