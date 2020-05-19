057e78d90cfe39d714e4f638e4af0dc1e9041be1
Nicoleta Ungur <ungurnicoleta1998@gmail.com>
2020-05-20 01:06:25 +0300

    Add migr

--- db/migrate/2020042213133454_create_follows.rb
+++ db/migrate/2020042213133454_create_follows.rb
@@ -1,19 +1,13 @@
 
-
-class CreateFollows < ActiveRecord::Migration[6.0]
-  def change
-    create_table :follows do |t|
-      t.integer :follower_id
-      t.integer :followee_id
-    end
-  end
-end
-
-
 #
-# class AddFollowCountersToUsers < ActiveRecord::Migration[6.0]
+# class CreateFollows < ActiveRecord::Migration[6.0]
 #   def change
-#     add_column :users, :followers_count, :integer, null: false, default: 0
-#     add_column :users, :followees_count, :integer, null: false, default: 0
+#     create_table :follows do |t|
+#       t.integer :follower_id
+#       t.integer :followee_id
+#     end
 #   end
 # end
+#
+
+
